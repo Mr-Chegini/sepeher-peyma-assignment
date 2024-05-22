3812ec02d9bd0f2668d0884c7f4a188a2f85d63d
Chegini <amirchegini007@gmail.com>
2024-05-22 04:18:43 +0330

    Implement rate limiting for more security

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ import ApiError from "./api/errors/ApiError.js";
 import openapiSpecification from "./swagger.js";
 import mongooseConnection from "./api/config/db.config.js";
 import apiErrorHandler from "./api/middlewares/apiErrorHandler.js";
+import rateLimiter from "./api/middlewares/rateLimiter.js";
 
 const PORT = 5000;
 const corsOptions = { origin: "http://localhost:4000" };
 
 const app = express();
-
+app.use(rateLimiter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -25,15 +26,13 @@ app.get("/", (req, res) => {
 });
 mongooseConnection();
 app.use("/api", router);
+
 // app.use((req, res, next) => {
 //   next(new ApiError.notFound("Route not found"));
 //   // const error = ApiError.notFound("Route not found");
 //   // next(error);
 // });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 app.use(apiErrorHandler);
 app.listen(PORT, () => {
   console.log(`
